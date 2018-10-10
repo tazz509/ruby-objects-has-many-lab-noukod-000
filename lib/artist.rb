b0f9e1449e83ded7d2b075df6ee5f74d92998a03
carly <carly@noukod.com>
2018-10-10 15:18:15 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,16 @@
 class Artist
   attr_accessor :name
- 
+
   def initialize(name)
     @name = name
     @songs = []
   end
- 
+
   def add_song(song)
     @songs << song
     song.artist = self
   end
- 
+
   def songs
     @songs
   end
---
+++ lib/author.rb
@@ -0,0 +1,18 @@
+class Author
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post (post)
+    @posts << post
+    post.author = self
+  end
+
+  def posts
+    @posts
+  end
+end

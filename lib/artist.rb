460b53717b82fe2da3523928eb16d42510cce081
carly <carly@noukod.com>
2018-10-10 15:32:22 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,15 +1,15 @@
 class Artist
   attr_accessor :name
   @songs = []
-  
+
   def initialize(name)
     @name = name
-  
+
     @songs << song
   end
 
   def add_song(song)
-    
+
     song.artist = self
   end
 
@@ -18,7 +18,11 @@ class Artist
     song.artist = self
   end
 
+  def songs
+     Song.all.select {|song| song.artist == self}
+  end
+
   def song_count
-    @songs
+       Song.all.count
   end
 end
